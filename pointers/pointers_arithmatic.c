d41545e21bed8fe5cfa270da8cb493d9f5adcd82
shazzad-hasan <shazzadraihan@gmail.com>
2023-09-28 12:21:42 +0100

    updated

---
+++ pointers/pointers_arithmatic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char *s = "Hi!";
+
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+
+    printf("%s\n", s);
+    printf("%s\n", s+1);
+    printf("%s\n", s+2);
+
+    return 0;
+}
--- pointers/pointers_demo.c
+++ pointers/pointers_demo.c
@@ -19,5 +19,14 @@ int main(){
     printf("Value of ptr is %p\n", ptr);
     printf("Value of the variable pointed by ptr is %d\n", *ptr);
 
+    char *s = "Hi!";
+    printf("%s\n", s);
+    printf("%p\n", s);
+    printf("%p\n", &s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+
     return 0;
 }
